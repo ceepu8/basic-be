af3f8d75fa7a6c57776350d1b973c4f633224879
ceepu8 <caophuonguyen132002@gmail.com>
2023-08-10 15:44:24 +0700

    feat(authorization): create login api

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -8,5 +8,5 @@ export const authentication = (salt: string, password: string) => {
   return crypto
     .createHmac("sha256", [salt, password].join("/"))
     .update(SECRET)
-    .digest();
+    .digest("hex");
 };
